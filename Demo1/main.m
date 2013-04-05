6ec877a8d15a2b2359229a1e5d4079d575e86ef2
Björn Dyvelcrona <bjorndyvelcrona@Bjorns-MacBook-Pro.local>
2013-04-05 11:06:35 +0200

    Initial Commit

---
+++ Demo1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Demo1
+//
+//  Created by Björn Dyvelcrona on 2013-04-05.
+//  Copyright (c) 2013 Björn Dyvelcrona. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
