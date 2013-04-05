1166218dda7a9bb0ba10845a6508c4a45d4e31be
Björn Dyvelcrona <bjorndyvelcrona@Bjorns-MacBook-Pro.local>
2013-04-05 11:09:26 +0200

    Added proj

---
+++ Demo1/DemoFile.h
@@ -0,0 +1,13 @@
+//
+//  DemoFile.h
+//  Demo1
+//
+//  Created by Björn Dyvelcrona on 2013-04-05.
+//  Copyright (c) 2013 Björn Dyvelcrona. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DemoFile : NSObject
+
+@end
---
+++ Demo1/DemoFile.m
@@ -0,0 +1,13 @@
+//
+//  DemoFile.m
+//  Demo1
+//
+//  Created by Björn Dyvelcrona on 2013-04-05.
+//  Copyright (c) 2013 Björn Dyvelcrona. All rights reserved.
+//
+
+#import "DemoFile.h"
+
+@implementation DemoFile
+
+@end
