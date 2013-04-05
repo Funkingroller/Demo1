0312a13d5200f350a93916101e07b2f6980fcb4a
Björn Dyvelcrona <bjorndyvelcrona@Bjorns-MacBook-Pro.local>
2013-04-05 11:18:53 +0200

    Added DemoFileX

---
+++ Demo1/DemoFileX.h
@@ -0,0 +1,13 @@
+//
+//  DemoFileX.h
+//  Demo1
+//
+//  Created by Björn Dyvelcrona on 2013-04-05.
+//  Copyright (c) 2013 Björn Dyvelcrona. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DemoFileX : NSObject
+
+@end
---
+++ Demo1/DemoFileX.m
@@ -0,0 +1,13 @@
+//
+//  DemoFileX.m
+//  Demo1
+//
+//  Created by Björn Dyvelcrona on 2013-04-05.
+//  Copyright (c) 2013 Björn Dyvelcrona. All rights reserved.
+//
+
+#import "DemoFileX.h"
+
+@implementation DemoFileX
+
+@end
